9c241e41c6d73b6958d8b80c1775864f0b97af9e
Ashwinigadad <ashwinigadad3@gmail.com>
2024-10-17 22:39:20 +0530

    server

--- Backend-node-mongo/src/index.js
+++ Backend-node-mongo/src/index.js
@@ -1,9 +1,13 @@
 import dotenv from 'dotenv';
-dotenv.config({ path: '/.env' });
+dotenv.config({ path: './.env.sample' }); 
 import connectDB from './db/db.js';
-
-import express from 'express';
-
+import express, { json } from 'express';
 const app = express();
+app.use(json());
+const PORT = process.env.PORT || 8000;
 
-connectDB();
+// connectDB();
+
+app.listen(PORT, () => {
+    console.log(`The server started on port ${PORT}`);
+});
