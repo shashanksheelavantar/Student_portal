c417c5002fe7d06a2adb5d8e8ba51cb485dce7f2
srujan BN <135449764+BNsrujan@users.noreply.github.com>
2024-10-20 11:52:23 +0530

    only froutnend

--- Backend-node-mongo/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-import {config} from 'dotenv';
-import { defineConfig } from "drizzle-kit";
-
-config({path:'.env'});
-
-export default defineConfig({
-    schema:"./src/models/*"
-})
--- Backend-node-mongo/src/models/user.model.js
+++
@@ -1,9 +0,0 @@
-
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
-export const usersTable = pgTable('users_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
-});
-
